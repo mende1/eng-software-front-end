3e667dfcbb9cfc6cbea7c7a595b4ee9d0492c96f
Gustavo Mendel <gustavo.mendel.br@gmail.com>
2022-07-03 11:38:16 -0300

    initial setup: axios and authorization services

---
+++ src/services/auth.js
@@ -0,0 +1,17 @@
+export const TOKEN_KEY = "@mata62-Token";
+
+export function isAuthenticated () { 
+  return localStorage.getItem(TOKEN_KEY) !== null
+}
+
+export function getToken() { 
+  return localStorage.getItem(TOKEN_KEY);
+}
+
+export function login(token) {
+  return localStorage.setItem(TOKEN_KEY, token);
+};
+
+export function logout() {
+  return localStorage.removeItem(TOKEN_KEY);
+};
